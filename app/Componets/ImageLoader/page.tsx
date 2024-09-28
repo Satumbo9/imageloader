8df5fc81034c4a9a07d48081d9269637edd81da3
Edimarf J. J. Satumbo <hello@satumbo9.com>
2024-09-28 06:27:22 -0700

    Exploring images and useStates, useEffects

--- app/Componets/ImageLoader/page.tsx
+++ app/Componets/ImageLoader/page.tsx
@@ -1,57 +1,25 @@
-"use client"
+// "use client";
+// import Image from "next/image";
+// import { CAR_MODELS } from "../const";
+// import React, { AllHTMLAttributes, forwardRef, ReactHTMLElement } from "react";
 
-import Image from 'next/image';
-import { CAR_MODELS } from '../const';
-import { useState, useEffect } from 'react';
-import Tesla from '../img/testla.avif';
+// interface dataType extends React.HTMLAttributes<HTMLAllCollection> {
+//   label: string;
+//   img: string,
+//   description: string,
+// }
 
-
-export default  function ImageLoader () { 
-
-    const [ImageLoaded, setImageLoaded] = useState(0);
-    const totalImages = CAR_MODELS.length;
-
-
-
-    function  handleLoader (){
-        console.log("Function Handler!!");
-        setImageLoaded(x =>{
-
-            return x+1;
-        } );
-        console.log("Current loaded images: " + ImageLoaded);
-
-        
-
-
-    }
-
-
-
-    return(
-        <div className="">
-            <p>Image Page Loader</p>
-
-            {CAR_MODELS.map((items, index)=>(
-                <div key={index}>
-                    <Image 
-                    // className='w-[400px] h-[400px]'
-                    onLoad={handleLoader}
-                    placeholder='blur'
-                    src={items.image}
-                    width={600}
-                    height={600}
-                    alt={items.name}
-                    priority
-                    
-                    />
-                    <p>{items.name}</p>
-
-
-                </div>
-
-            ))
-            }
-        </div>
-    )
-}
+// export const ImageLoader = forwardRef<HTMLAllCollection, dataType>({
+//   label,
+//   img,
+//   description,
+//   h,
+//   w,
+//   url,
+// }) => {
+//   return (
+//     <div>
+//       <Image src={url} alt={`${description}`} height={h} width={w} />
+//     </div>
+//   );
+// };
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
-import ImageLoader   from "./Componets/ImageLoader/page";
+// import ImageLoader from "./Componets/ImageLoader/page";
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center">
-        <ImageLoader />
+      <p>Hello ImageLoader</p>
+      {/* <ImageLoader /> */}
     </div>
   );
 }
